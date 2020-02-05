e657a08941717dc3c47889fb079057517d0f9fc5
Ryan Bennett <ryanbennettvoid@gmail.com>
2020-02-05 15:53:52 +0700

    normalizes data

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 
-// const dataUrl = `https://docs.google.com/spreadsheets/d/1wQVypefm946ch4XDp37uZ-wartW4V7ILdg-qYiDXUHM/htmlview?usp=sharing&sle=true#`
-
 const fs = require('fs')
 const fetch = require('node-fetch')
 const tempfile = require('tempfile')
-const XLSX = require('xlsx');
+const XLSX = require('xlsx')
+const csvParse = require('csv-parse/lib/sync')
+const moment = require('moment')
 
 const SHEET_ID = '1wQVypefm946ch4XDp37uZ-wartW4V7ILdg-qYiDXUHM'
 const sheetUrl = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/export?format=xlsx`
+
+const parseLastUpdate = (lastUpdate, sheetName) => {
+  const normalizedSheetName = sheetName.substr(0, 5)
+  try {
+    if (lastUpdate.length === '2/4/20 3:03'.length) {
+      return moment(moment(lastUpdate).format('MM/DD/YY HH:mm')).toISOString()
+    } else if (lastUpdate.length === '2/4/2020 3:03'.length) {
+      return moment(moment(lastUpdate).format('MM/DD/YYYY HH:mm')).toISOString()
+    } else if (lastUpdate.length === '2/4/2020 3:03:00'.length) {
+      return moment(moment(lastUpdate).format('MM/DD/YYYY HH:mm:ss')).toISOString()
+    }
+    const fallbackDate = moment(moment(normalizedSheetName).format('MMMDD'))
+    fallbackDate.set('year', 2020)
+    return fallbackDate.toISOString()
+  } catch (err) {
+    console.error(err, { lastUpdate, normalizedSheetName })
+    return null
+  }
+}
 
 const fetchData = async () => {
   try {
@@ -19,12 +38,28 @@ const fetchData = async () => {
     fs.writeFileSync(filepath, buffer)
 
     const workbook = XLSX.readFile(filepath)
-    const { SheetNames, Sheets } = workbook
-    for (sheetName of SheetNames) {
-      const sheet = XLSX.utils.sheet_to_json(Sheets[sheetName])
-      console.log(JSON.stringify(sheet, null, 2))
-      fs.writeFileSync(`${sheetName}.json`, JSON.stringify(sheet, null, 2))
-    }
+    const { SheetNames: sheetNames, Sheets: sheets } = workbook
+
+    return sheetNames.reduce((outerAcc, sheetName) => {
+      const sheetAsCsv = XLSX.utils.sheet_to_csv(sheets[sheetName])
+      const sheetAsJson = csvParse(sheetAsCsv)
+      const sheetData = sheetAsJson.reduce((acc, line, idx) => {
+        if (idx === 0) {
+          return acc // skip labels
+        }
+        const [ province, country, lastUpdate, confirmed, deaths, recovered ] = line
+        const parsedDate = parseLastUpdate(lastUpdate, sheetName)
+        return acc.concat({
+          province,
+          country,
+          lastUpdate: parsedDate,
+          confirmed,
+          deaths,
+          recovered
+        })
+      }, [])
+      return outerAcc.concat(sheetData)
+    }, [])
 
   } catch (err) {
     return Promise.reject(err)
@@ -33,7 +68,8 @@ const fetchData = async () => {
 
 const main = async () => {
   try {
-  const data = await fetchData()  
+  const data = await fetchData()
+  fs.writeFileSync(`./dump-${Date.now()}.json`, JSON.stringify(data, null, 2))
   } catch (err) {
     return Promise.reject(err)
   }
