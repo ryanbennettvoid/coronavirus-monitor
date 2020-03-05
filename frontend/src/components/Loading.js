4a4ff8a53f7b6b1752d8d987011620cf739b7c9e
Ryan Bennett <ryanbennettvoid@gmail.com>
2020-03-05 16:30:48 +0700

    bigger loading text

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import useInterval from 'react-useinterval'
+import './Loading.css'
 
 export default function Loading() {
   const [count, setCount] = useState(0)
@@ -14,5 +15,5 @@ export default function Loading() {
 
   const message = count < 10 ? 'Loading' : 'This may take a few seconds'
   const dots = Array(count).fill('.')
-  return <div>{message}{dots}</div>
+  return <div className='loading-text'>{message}{dots}</div>
 }
