56f2f6ec3c38abf65391e2699a90e70a84f99b08
Ryan Bennett <ryanbennettvoid@gmail.com>
2020-03-28 11:57:17 +0700

    add cache

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,10 @@
 
 const moment = require('moment')
 const { getHistory, getGeo } = require('./data-fetcher2')
+const Cache = require('./cache')
+const cache = Cache.newInstance(1000 * 60 * 60)
+
+const HISTORY_CACHE_KEY = 'history'
 
 module.exports = [
   {
@@ -9,8 +13,18 @@ module.exports = [
     handlers: [
       async (req, res) => {
         try {
+          if (!cache.isExpired()) {
+            const data = cache.get(HISTORY_CACHE_KEY)
+            if (data) {
+              res.json(data)
+              return
+            }
+          }
+
           const data = await getHistory()
           res.json(data)
+
+          cache.set(HISTORY_CACHE_KEY, data)
         } catch (err) {
           console.error(err)
           res.status(500).json({ error: err.message })
