336082221afa9356a11a0862733d7cfb0b183f1e
Ryan Bennett <ryanbennettvoid@gmail.com>
2020-04-04 22:25:55 +0700

    start on tab 0

--- frontend/src/components/TabSwitcher.js
+++ frontend/src/components/TabSwitcher.js
@@ -5,7 +5,7 @@ function TabSwitcher(props) {
 
   const { tabs } = props
 
-  const [currentTabIndex, setCurrentTabIndex] = useState(1)
+  const [currentTabIndex, setCurrentTabIndex] = useState(0)
 
   return (
     <div className="tabswitcher">
