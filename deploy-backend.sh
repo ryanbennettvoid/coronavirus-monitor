0494b33d0a45b7cdd4528f7ae44861e45920dd6a
Ryan Bennett <ryanbennettvoid@gmail.com>
2020-04-04 18:06:03 +0700

    backend script update

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-git subtree push --prefix backend heroku master
+# git subtree push --prefix backend heroku master
+git push heroku `git subtree split --prefix backend master`:master --force
